efa1fb7a190bf3b4f5e8d65e9dae5ad958f3c800
Arshad Nazeer <arshadnhusain@gmail.com>
2026-03-12 15:38:46 +0530

    bruv

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
 //   baseURL: "http://localhost:3600/api"
-    baseURL: "/api"
+    // baseURL: "/api"
+    baseURL: "https://store-management-system-pmpu.onrender.com/api"
 });
 
 export default api;
